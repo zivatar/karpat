4f13bc953e15462b00a3f8fa30afd77df2ba30ad
MacGyver <macgyver1024@gmail.com>
2017-07-28 22:05:25 +0200

    Routing was fixed

--- app/mainCtrl/main.js
+++ app/mainCtrl/main.js
@@ -1,15 +1,8 @@
 'use strict';
 
-// angular.module('myApp.mainPage', ['ngRoute'])
+angular.module('myApp.mainCtrl', [])
 
-// .config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.when('/index', {
-//     templateUrl: 'view1/view1.html',
-//     controller: 'mainPageCtrl'
-//   });
-// }])
-var myApp = angular.module('myApp',[]);
-myApp.controller('mainCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+.controller('mainCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
 	$scope.main = "ABC";
 	$rootScope.Constant = Constant;
 }]);
