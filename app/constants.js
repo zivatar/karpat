0f23c6a54187311f176895e99e503bcb21764791
MacGyver <macgyver1024@gmail.com>
2017-07-29 20:48:48 +0200

    Comments

--- app/constants.js
+++ app/constants.js
@@ -18,7 +18,10 @@ Dummy.MENU = [
 Dummy.MAIN_IMG = [
 	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg"},
 	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg"},
-	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok1.jpg"}
+	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok1.jpg"},
+	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg"},
+	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg"},
+	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok1.jpg"},
 ];
 
 Dummy.ARTICLE = {
@@ -30,4 +33,10 @@ Dummy.ARTICLE = {
 	josvafo2: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
 	aggtelek3: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
 	josvafo4: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-}
+};
+
+Dummy.COMMENTS = [
+	{name: "jozsi", date: "2015.01.01. 12:12", text: "vjvjdo pdovjvdp vdpoj vpdojdv pdoj vdopvjd povjd opv djvdp odv djopdv dopdj vdpojdjv dpo vjdopvdj odjv dopodpvj dpdov dpvdo", email: "macgyver1024@gmail.com"},
+	{name: "jozsi", date: "2015.01.01. 12:12", text: "vjvjdo pdovjvdp vdpoj vpdojdv pdoj vdopvjd povjd opv djvdp odv djopdv dopdj vdpojdjv dpo vjdopvdj odjv dopodpvj dpdov dpvdo", email: "macgyver1023@gmail.com"},
+	{name: "jozsi", date: "2015.01.01. 12:12", text: "vjvjdo pdovjvdp vdpoj vpdojdv pdoj vdopvjd povjd opv djvdp odvdjopdv dopdj vdpojdjv dpo vjdopvdj odjv dopodpvj dpdov dpvdo", email: ""},
+];
