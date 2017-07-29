8ca0d133365390f766b7160fe3f4d9fe548ff6d5
MacGyver <macgyver1024@gmail.com>
2017-07-29 15:47:31 +0200

    Loading spinner, main page improvement

--- app/constants.js
+++ app/constants.js
@@ -22,6 +22,12 @@ Dummy.MAIN_IMG = [
 ];
 
 Dummy.ARTICLE = {
-	aggtelek: {title: "Aggtelek", text: "klk"},
-	josvafo: {title: "Jósvafő", text: "jjkjk"}
+	aggtelek: {title: "Aggtelek ikh kh dkshdskdshks hdskdhksshkshd", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek1: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo1: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek2: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo2: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek3: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo4: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
 }
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,6 @@ angular.module('myApp.mainPage', ['ngRoute'])
   });
 }])
 
-.controller('mainPageCtrl', [function() {
+.controller('mainPageCtrl', ['$scope', function($scope) {
 
 }]);
