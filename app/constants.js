72b738efe908bc228db90432d8a162b29d0ca0c4
hagelgewitter <hagelgewtter@gmail.com>
2017-07-30 00:32:55 +0200

    Fokepernyo kiskep filenevek modositasa

--- app/constants.js
+++ app/constants.js
@@ -16,23 +16,23 @@ Dummy.MENU = [
 ];
 
 Dummy.MAIN_IMG = [
-	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg"},
-	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg"},
-	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok1.jpg"},
-	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg"},
-	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg"},
-	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok1.jpg"},
+	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak_212px.jpg"},
+	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok_212px.jpg"},
+	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok_212px.jpg"},
+	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak_212px.jpg"},
+	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok_212px.jpg"},
+	{text: "Geodéziai mérőtornyok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/geodeziai-merotornyok_212px.jpg"},
 ];
 
 Dummy.ARTICLE = {
-	aggtelek: {title: "Aggtelek ikh kh dkshdskdshks hdskdhksshkshd", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	aggtelek1: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo1: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	aggtelek2: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo2: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	aggtelek3: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo4: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek: {title: "Aggtelek ikh kh dkshdskdshks hdskdhksshkshd", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek1: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo1: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek2: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo2: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek3: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	josvafo4: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak_212px.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
 };
 
 Dummy.COMMENTS = [
