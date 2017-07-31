3020fb36e1c6425f6e702c075a08c3e0a8735dfd
MacGyver <macgyver1024@gmail.com>
2017-07-31 22:53:00 +0200

    Dummy data for article

--- app/constants.js
+++ app/constants.js
@@ -5,17 +5,6 @@ Constant.API_URL = "http://www.zivatar.hu/api.php";
 
 Dummy = {};
 
-Dummy.MENU = [
-	{ id: "epitett", text: "Épített értékek", submenu: 
-		[ { id: "ipari", text: "Ipari létestímények"}, { id: "muzeum", text: "Múzeumok"}, { id: "var", text: "Várak"}
-		] },
-	{
-		id: "termeszet", text: "Természeti értékek", submenu: 
-		[ { id: "ipari", text: "Ipari létestímények"}, { id: "muzeum", text: "Múzeumok"}, { id: "var", text: "Várak"}
-		]
-	}
-];
-
 Dummy.MAIN_IMG = [
 	{text: "Bányák", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg"},
 	{text: "Barlangok", url: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg"},
@@ -26,14 +15,28 @@ Dummy.MAIN_IMG = [
 ];
 
 Dummy.ARTICLE = {
-	aggtelek: {title: "Aggtelek ikh kh dkshdskdshks hdskdhksshkshd", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	aggtelek1: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo1: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	aggtelek2: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo2: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	aggtelek3: {title: "Aggtelek", text: "klk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
-	josvafo4: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/aggtelek"},
+	aggtelek: {
+		title: "Aggtelek ikh", 
+		text: "klk", 
+		img: "http://www.karpat-medence.hu/img/ikonok_fooldal/barlangok1.jpg", 
+		url:"http://localhost:8000/#!/cikk/aggtelek", 
+		foreignName: null, 
+		text: "mkfdm kdfdmfldkm fdlmfdlmfd lkdmfldkm flkdmf dlkfdmdlkmfd lkdmf dkldmkfdl", 
+		galleryUrl: "http://www.karpat-medence.hu/keptar/index.php?/category/1564", 
+		sources: "fdélk édkfdékfd éf", 
+		vmID: 'bekes', 
+		mID: 'pest', 
+		cityID: 'miskolc', 
+		createdDate: Date.now(), 
+		modifiedDate: Date.npw(), 
+		user: {name: "jannesz", email: "hagelgewitter@gmail.com"}, 
+		coordinate: {lat: 47, lon: 19}, 
+		comments: [
+			{name: "jozsi", date: "2015.01.01. 12:12", text: "vjvjdo pdovjvdp vdpoj vpdojdv pdoj vdopvjd povjd opv djvdp odv djopdv dopdj vdpojdjv dpo vjdopvdj odjv dopodpvj dpdov dpvdo", email: "macgyver1024@gmail.com", url:"http://localhost:8000/#!/cikk/aggtelek", articleTitle:"Halott ejtőernyősök emlékműve"},
+			{name: "jozsi", date: "2015.01.01. 12:12", text: "vjvjdo pdovjvdp vdpoj vpdojdv pdoj vdopvjd povjd opv djvdp odv djopdv dopdj vdpojdjv dpo vjdopvdj odjv dopodpvj dpdov dpvdo", email: "macgyver1023@gmail.com", url:"http://localhost:8000/#!/cikk/aggtelek", articleTitle:"Óbudai esernyős szobor"},
+			{name: "jozsi", date: "2015.01.01. 12:12", text: "vjvjdo pdovjvdp vdpoj vpdojdv pdoj vdopvjd povjd opv djvdp odvdjopdv dopdj vdpojdjv dpo vjdopvdj odjv dopodpvj dpdov dpvdo", email: "", url:"http://localhost:8000/#!/cikk/aggtelek", articleTitle:"Aggtelek"},
+		]},
+	josvafo: {title: "Jósvafő", text: "jjkjk", img: "http://www.karpat-medence.hu/img/ikonok_fooldal/banyak1.jpg", url:"http://localhost:8000/#!/cikk/josvafo"},
 };
 
 Dummy.COMMENTS = [
